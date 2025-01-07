3ec1780cd761276a18e83712f1ed98a0acf6cb0f
Shane <shane.everitt.murphy@gmail.com>
2025-01-06 18:32:23 -0800

    Add gdUnit4 support

--- scripts/setup.cmd
+++ scripts/setup.cmd
@@ -2,9 +2,9 @@
 setlocal
 
 for /f "delims=" %%i in ('git rev-parse --show-toplevel') do set ROOT=%%i
-@rem Replace forward with backward slashes in ROOT
 set ROOT=%ROOT:/=\%
 set GODOT_VERSION=4.3-stable
+set GDUNIT_VERSION=4.5.0
 
 set dir=%TEMP%\godot
 mkdir %dir%
@@ -16,5 +16,11 @@ powershell -command "Expand-Archive -Path '%zip%' -DestinationPath '%ROOT%\.bin\
     echo @echo off
     echo start /D %ROOT%\.bin %ROOT%\.bin\Godot_v%GODOT_VERSION%_mono_win64\Godot_v%GODOT_VERSION%_mono_win64.exe
 ) > "%ROOT%\.bin\godot.cmd"
+
+set zip=%dir%\gdunit.zip
+curl -o "%zip%" -L https://github.com/MikeSchulze/gdUnit4/archive/refs/tags/v%GDUNIT_VERSION%.zip
+powershell -command "Expand-Archive -Path '%zip%' -DestinationPath '%dir%/gdunit'"
+mkdir %ROOT%\addons
+move %dir%\gdunit\gdUnit4-%GDUNIT_VERSION%\addons\gdUnit4 %ROOT%\addons\gdUnit4
 
 endlocal
